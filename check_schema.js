42349587c601b1704879e07126300a38967a8d59
Krishna Sathwik.R <rallabandibunny@gmail.com>
2026-05-21 14:07:21 +0530

    Delete check_schema.js

--- check_schema.js
+++
@@ -1,7 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('governtogether.db');
-db.all("PRAGMA table_info(users)", (err, rows) => {
-    if (err) console.error(err);
-    console.log(JSON.stringify(rows, null, 2));
-    db.close();
-});
